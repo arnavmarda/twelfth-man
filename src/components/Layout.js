8d7340b213218ebcef582a8e4fd2ecb86e72cf55
arnavmarda <arnavmarda@gmail.com>
2022-11-25 15:14:17 -0800

    Restructuring.

--- src/components/Layout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
-export const Layout = (props) => (
-    <Container fluid className='p-0'>
-        {props.children}
-    </Container>
-)
