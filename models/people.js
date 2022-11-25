d4b3a26de43c0e2c0d01b4a0c608c418fd2f06b8
arnavmarda <arnavmarda@gmail.com>
2022-11-24 23:05:26 -0800

    Restructured project

--- models/people.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var personSchema = new Schema({
-    name: String,
-    age: Number
-});
-
-module.exports = mongoose.model('People', personSchema);
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-// Load config
-const config = require('config');
-var serverConfig = config.get('sampleapp.server');
-
-exports.index = function (req, res) {
-    console.log("[INFO] /index");
-    res.render('index', { help_text: serverConfig.help_text });
-};
