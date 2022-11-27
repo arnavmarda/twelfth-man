c2db6cb805d6dfc12209e45bbd00787564f01384
Angad Kapoor <angadk0810@gmail.com>
2022-11-26 18:18:32 -0800

    added check for whether captain exists as User

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const requireLogin = require("../middleware/requireLogin.js");
+const User = require("../models/User.js");
 
+const User = mongoose.model("User");
 const Team = mongoose.model("Team");
 
 /*
@@ -29,43 +31,35 @@ router.post("/team/create", requireLogin, (req, res) => {
         });
     }
 
-    Team.findOne({ name: name })
-        .then((savedTeam) => {
-            if (savedTeam) {
-                return res.status(422).json({
-                    error: "Team with this name already exists",
-                });
-            }
+    User.findOne({ name: captain }).then((player) => {
+        if (player) {
+            Team.findOne({ name: name })
+                .then((savedTeam) => {
+                    if (savedTeam) {
+                        return res.status(422).json({
+                            error: "Team with this name already exists",
+                        });
+                    }
 
-            const newTeam = new Team({
-                name: name,
-                captain: captain,
-                roster: roster ? roster : Array(11),
-            });
+                    const newTeam = new Team({
+                        name: name,
+                        captain: player,
+                        roster: roster ? roster : Array(11),
+                    });
 
-            newTeam
-                .save()
-                .then((team) => res.json(team))
+                    newTeam
+                        .save()
+                        .then((team) => res.json(team))
+                        .catch((err) => console.log(err));
+                })
                 .catch((err) => console.log(err));
-        })
-        .catch((err) => console.log(err));
-});
-
-/*
-    Update a team
-*/
-router.patch("/team/update/:id", requireLogin, (req, res) => {
-    const { name, captain, roster } = req.body;
-    Team.findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: { name, captain, roster } }
-    )
-        .then((response) => res.status(200).json(response))
-        .catch((err) => {
+        } else {
             return res.status(400).json({
-                error: "Error updating team",
-            });
-        });
+                error: "No player found with that name. Please register before trying again.",
+            })
+        }
+    })
+    .catch((err) => console.log(err));
 });
 
 /*
