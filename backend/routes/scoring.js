9bdd9eb5b1ec883f0dbcc67a1377db5031daf72a
Tanmay Desai <desai.j.tanmay@gmail.com>
2022-11-28 03:06:56 -0800

    Created LegBye Function: deal with Scoring legbyes

--- backend/routes/scoring.js
+++ backend/routes/scoring.js
@@ -10,6 +10,7 @@ function takeSingle(OverUpdate)
 {
    let OneRun = '1';
    OverUpdate.concat(' ', OneRun);
+   return OverUpdate;
    //Test: console.log("Hello World");
 }
  
@@ -17,36 +18,42 @@ function takeDouble(OverUpdate)
 {
    let TwoRuns = '2';
    OverUpdate.concat(' ', TwoRuns);
+   return OverUpdate;
 }
  
 function takeTriple(OverUpdate)
 {
    let ThreeRuns = '3';
    OverUpdate.concat(' ', ThreeRuns);
+   return OverUpdate;
 }
  
 function Boundary(OverUpdate)
 {
    let FourRuns = '4';
    OverUpdate.concat(' ', FourRuns);
+   return OverUpdate;
 }
  
 function takeFive(OverUpdate)
 {
    let TwoRuns = '5';
    OverUpdate.concat(' ', takeFive);
+   return OverUpdate;
 }
  
 function Sixer(OverUpdate)
 {
    let SixRuns = '6';
    OverUpdate.concat(' ', SixRuns);
+   return OverUpdate;
 }
 
 function dotBall(OverUpdate)
 {
     let NoRun = '0';
     OverUpdate.concat(' ', NoRun);
+    return OverUpdate;
 }
 
 //Creating miscellaneous runs scoring methods 
@@ -55,6 +62,7 @@ function wideBall(OverUpdate)
 {
     let wideRun = 'E';
     OverUpdate.concat(' ', wideRun)
+    return OverUpdate;
 }
 
 //Needed to create a 7Runs function incase of a noball 6
@@ -65,10 +73,9 @@ function noBall(OverUpdate, RunScoredOnThatBall)
 {
     let NoBallRun = 'N'; 
     //N represents No Ball
-
     //We need to now check to see what the batsman scored and then increment the score by 1
     //So if he scored 1 on the no ball I would update the string with 2 to account for the noball 1
-
+    return OverUpdate;
 }
 
 function legBye(OverUpdate, RunsScored)
@@ -78,6 +85,35 @@ function legBye(OverUpdate, RunsScored)
     //Leg byes are when a batsman does not hit the ball with his bat, but
     //is able to take a number of run(s). 
     
+    let legByeRuns = 'LB';
+    switch(RunsScored)
+    {
+        case '2': 
+            legByeRuns = 'LB2';
+            OverUpdate.concat(' ', legByeRuns); 
+            break; 
+        case '3': 
+            legByeRuns = 'LB3';
+            OverUpdate.concat(' ', legByeRuns); 
+            break; 
+        case '4': 
+            legByeRuns = 'LB4';
+            OverUpdate.concat(' ', legByeRuns); 
+            break; 
+        case '5': 
+            legByeRuns = 'LB5';
+            OverUpdate.concat(' ', legByeRuns); 
+            break; 
+        case '6': 
+            legByeRuns = 'LB6';
+            OverUpdate.concat(' ', legByeRuns); 
+            break; 
+        default: 
+            legByeRuns = 'LB1';
+            OverUpdate.concat(' ', legByeRuns)
+            break; 
+    }
+    return OverUpdate;
 }
 
 
