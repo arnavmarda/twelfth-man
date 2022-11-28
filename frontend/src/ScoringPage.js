1d69f2f72cd4ebc32aa203afde5ac3b97c0af8f0
arnavmarda <arnavmarda@gmail.com>
2022-11-28 01:09:59 -0800

    Completed Scoring Page graphics

--- frontend/src/ScoringPage.js
+++ frontend/src/ScoringPage.js
@@ -3,7 +3,13 @@ import styled from "styled-components";
 import { Jumbotron } from "./components/JumbotronScoring";
 import { Footer } from "./components/Footer";
 import { Layout } from "./components/Layout";
+import { RenderOver } from "./components/OverGraphics";
 import { Container, Row, Col, Table } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { ToggleButtonGroup, ToggleButton } from "react-bootstrap";
+import { ModalScoring } from "./components/ModalScoring";
+import { ModalOver } from "./components/ModalOver";
+import { ModalWicket } from "./components/ModalWicket";
 
 const Styles = styled.div`
     .bg {
@@ -35,11 +41,19 @@ const Styles = styled.div`
         border-width: normal;
     }
 
+    .over {
+        border-color: black;
+        border-width: thick;
+        border-style: solid;
+    }
+
     .headers {
         background-color: #FFB81C;
         border-style: solid;
         border-width: thick;
         border-color: #003B5C;
+        color: black;
+        font-weight: bold;
     }
 
     hr.solid {
@@ -47,15 +61,147 @@ const Styles = styled.div`
         width: 100%;
         margin: 0;
     }
+
+    .text-trial {
+        color: white;
+    }
+
+    .ball-button {
+        border-style: solid;
+        border-color: black;
+        border-width: thin;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .dot {
+        background-color: grey;
+    }
+
+    .one {
+        background-color: purple;
+    }
+
+    .four, .five {
+        background-color: green;
+    }
+
+    .six {
+        background-color: blue;
+    }
+
+    .wicket {
+        background-color: red;
+    }
+
+    .extras {
+        background-color: turquoise;
+    }
+
+    .text {
+        color: white;
+    }
+
+    .ball-button {
+        border-style: solid;
+        border-color: black;
+        border-width: thin;
+        -webkit-border-radius: 50%;
+        border-radius: 50% !important;
+        padding: 5px 15px;
+        height: 30px;
+        width: auto;
+        text-align: center;
+        vertical-align: center;
+    }
+
+    .tr-no-border {
+        border-color: #FFD100 !important;
+    }
+
+    .btn {
+        text-align: center;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .submit-buttons {
+        background-color: blue;
+        border-width: normal;
+        border-color: black;
+    }
+
+    .blinking-live-icon {
+        background-color: #1c87c9;
+        -webkit-border-radius: 50%;
+        border-radius: 50%;
+        border: none;
+        color: #eeeeee;
+        display: inline-block;
+        padding: 5px 8px;
+        text-align: center;
+        text-decoration: none;
+        // top: 50%;
+        position: relative;
+        height: 16px;
+        width: auto;
+        margin: 0;
+        vertical-align: middle;
+    }
+
+    @keyframes glowing {
+        0% {
+          background-color: #2ba805;
+        }
+        50% {
+          background-color: black;
+        }
+        100% {
+          background-color: #2ba805;
+        }
+    }
+
+    .blinking-live-icon {
+        animation: glowing 1000ms infinite;
+    }
+
+    .header-main {
+        height: 50px;
+        font-size: 25px;
+    }
 `;
 
 class ScoringPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentBall: "",
+            callOverEnd: false,
+        };
+
+        this.handleEndOver = this.handleEndOver.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    handleEndOver(event) {
+        this.setState({callOverEnd: true})
+    }
+
+    handleSave(event) {
+        
+    }
+
     render() {
         return (
             <Styles>
                 <Layout>
+                    <ModalScoring />
                     <Jumbotron />
-                    <Container fluid className="bg text pb-5">
+                    <Container fluid className="bg text pb-5 pt-5">
+                        <Container fluid className="headers mt-3 mb-0 header-main">
+                            <p className="blinking-live-icon"></p>
+                            <h fluid className="vertical-align-middle"> INNINGS 1 - TEAM A</h>
+                        </Container>
                         <Container className="pt-5 pb-1">
                             <Row>
                                 <Col className="ms-3">
@@ -120,11 +266,63 @@ class ScoringPage extends React.Component {
                                             </tbody>
                                         </Table>
                                         <hr className="solid"></hr>
+                                        <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Current Over'} overnumber={0} />
+                                        <hr className="solid"></hr>
+                                        <Table className="mb-0 mt-3">
+                                            <tbody className="tbody">
+                                                <tr>
+                                                    <ToggleButtonGroup type="radio" name="runs" id="runs" defaultValue={"0"}>
+                                                        <ToggleButton value={"0"} id="tbg-radio-1" className="rounded-circle dot" name="runs">.</ToggleButton>
+                                                        <ToggleButton value={"1"} id="tbg-radio-2" className="rounded-circle one" name="runs">1</ToggleButton>
+                                                        <ToggleButton value={"2"} id="tbg-radio-3" className="rounded-circle one" name="runs">2</ToggleButton>
+                                                        <ToggleButton value={"3"} id="tbg-radio-4" className="rounded-circle one" name="runs">3</ToggleButton>
+                                                        <ToggleButton value={"4"} id="tbg-radio-5" className="rounded-circle four" name="runs">4</ToggleButton>
+                                                        <ToggleButton value={"5"} id="tbg-radio-6" className="rounded-circle four" name="runs">5</ToggleButton>
+                                                        <ToggleButton value={"6"} id="tbg-radio-7" className="rounded-circle six" name="runs">6</ToggleButton>
+                                                        <ToggleButton value={"W"} id="tbg-radio-8" className="rounded-circle wicket" name="runs">W</ToggleButton>
+                                                    </ToggleButtonGroup>
+                                                </tr>
+                                            </tbody>
+                                        </Table>
+                                        <Table className="mt-0 mb-0">
+                                            <tbody className="tbody">
+                                                <tr>
+                                                    <ToggleButtonGroup type="radio" name="extras" id="extras" defaultValue={null}>
+                                                        <ToggleButton value={null} id="extras-radio-4" className="rounded-circle extras" name="extras">N/A</ToggleButton>
+                                                        <ToggleButton value={"Wd"} id="extras-radio-3" className="rounded-circle extras" name="extras">Wd</ToggleButton>
+                                                        <ToggleButton value={"B"} id="extras-radio-2" className="rounded-circle extras" name="extras">B</ToggleButton>
+                                                        <ToggleButton value={"NB"} id="extras-radio-1" className="rounded-circle extras" name="extras">NB</ToggleButton>
+                                                    </ToggleButtonGroup>
+                                                </tr>
+                                            </tbody>
+                                        </Table>
+                                        <Table className="mt-0 mb-3">
+                                            <tbody className="tbody">
+                                                <tr>
+                                                    <Button type="submit" className="submit-buttons" variant="primary">Save</Button>
+                                                    <Button type="submit" className="submit-buttons" onClick={this.handleEndOver} variant="primary">End Over</Button>
+                                                    {this.state.callOverEnd ? <ModalOver /> : <input type="hidden"></input>}
+                                                    <Button type="submit" className="submit-buttons" variant="primary">End Innings</Button>
+                                                </tr>
+                                            </tbody>
+                                        </Table>
                                     </Container>
                                 </Col>
                                 <Col className="ms-3">
                                     <Container fluid className="headers m-3">
                                         <h fluid>OVER BY OVER</h>
+                                    </Container>
+                                    
+                                    <Container className="col-border m-3">
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 0'} overnumber={0} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 1'} overnumber={1} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 2'} overnumber={2} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 3'} overnumber={3} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 4'} overnumber={4} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 5'} overnumber={5} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 6'} overnumber={6} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 7'} overnumber={7} />
+                                    <RenderOver balls={["1", "2", "1", "1", "1", "1"]} over={'Over 8'} overnumber={8} />
                                     </Container>
                                 </Col>
                             </Row>
---
+++ frontend/src/components/ModalOver.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Modal } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
+const Styled = styled.div`
+`;
+
+export const ModalOver = (props) => {
+
+    const [isShown, setIsShown] = useState(props.toShow);
+
+    const showModal = () => {
+        setIsShown(true);
+    }
+
+    const closeModal = () => {
+        setIsShown(false);
+    }
+
+    return (
+        <Modal autoFocus centered show={isShown} onExit={closeModal} onExited={closeModal}>
+            <Modal.Header>
+                <Modal.Title>Choose Next Bowler</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form> 
+                    <Form.Group className="mb-3" controlId="chooseBatsmanOut">
+                        <Form.Select required aria-label="Next Bowler" className="mb-2">
+                            <option>Next Bowler</option>
+                            <option value="Bowler 1">Bowler 1</option>
+                            <option value="Bowler 2">Bowler 2</option>
+                            <option value="Bowler 3">Bowler 3</option>
+                            <option value="Bowler 4">Bowler 4</option>
+                            <option value="Bowler 5">Bowler 5</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button type="submit" onClick={closeModal}>Continue!</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+};
