cb72a6f85a3bdd2bb912ef6850aaf6ec193ed6e6
Tanmay Desai <desai.j.tanmay@gmail.com>
2022-11-28 01:47:20 -0800

    Created Baseline Functions for Run Updates

--- backend/routes/scoring.js
+++ backend/routes/scoring.js
@@ -1,1 +1,50 @@
-let Score = ''
+let Score = '';
+
+function takeSingle(OverUpdate)
+{
+   let OneRun = '1';
+   OverUpdate.concat(" ", OneRun);
+   console.log("Hello World");
+}
+ 
+function takeDouble(OverUpdate)
+{
+   let TwoRuns = '2';
+   OverUpdate.concat(" ", TwoRuns);
+}
+ 
+function takeTriple(OverUpdate)
+{
+   let ThreeRuns = '3';
+   OverUpdate.concat(" ", ThreeRuns);
+}
+ 
+function Boundary(OverUpdate)
+{
+   let FourRuns = '4';
+   OverUpdate.concat(" ", FourRuns);
+}
+ 
+function takeFive(OverUpdate)
+{
+   let TwoRuns = '5';
+   OverUpdate.concat(" ", takeFive);
+}
+ 
+function Sixer(OverUpdate)
+{
+   let SixRuns = '6';
+   OverUpdate.concat(" ", SixRuns);
+}
+
+function dotBall(OverUpdate)
+{
+    let NoRun = '0';
+    OverUpdate.concat(" ", NoRun);
+}
+
+
+
+
+
+console.log("hello")
