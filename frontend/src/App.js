1562d8b03a5e88cf454e4b76af898486a7196b75
arnavmarda <arnavmarda@gmail.com>
2022-11-27 17:17:03 -0800

    Additions to match page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import Home from "./Home";
 import UserPage from "./UserPage";
 import TournamentPage from "./TournamentPage";
 import TeamPage from "./TeamPage";
+import MatchPage from "./MatchPage";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScoringPage from "./ScoringPage";
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path="/user" element={<UserPage />} />
           <Route path="/tournament" element={<TournamentPage />} />
           <Route path="/team" element={<TeamPage />} />
+          <Route path="/scoring" element={<ScoringPage />} />
+          <Route path="/match" element={<MatchPage />} />
         </Routes>
       </Router>
     </React.Fragment>
