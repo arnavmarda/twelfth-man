2b4c0ba8133b90edbe0e8d685c354db8ae409fab
arnavmarda <arnavmarda@gmail.com>
2022-11-27 17:16:55 -0800

    Scoring Page (not complete)

---
+++ frontend/src/components/OverGraphics.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import { Button, ButtonGroup, Container, Stack } from 'react-bootstrap';
+
+const Styles = styled.div`
+
+    .dot {
+        color: grey;
+    }
+
+    .one, .two, .three {
+        color: purple;
+    }
+
+    .four, .five {
+        color: green;
+    }
+
+    .six {
+        color: blue;
+    }
+
+    .wicket {
+        color: red;
+    }
+
+    .extras {
+        color: orange;
+    }
+
+    .text {
+        color: white;
+    }
+
+    .ball-button {
+        border-style: solid;
+        border-color: black;
+        border-width: thin;
+    }
+
+`;
+
+function DecideColor(props) {
+    
+}
+
