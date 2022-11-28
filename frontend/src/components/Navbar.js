ad13046b8e097921a9786f4b779d8d63a4886867
arnavmarda <arnavmarda@gmail.com>
2022-11-28 02:07:45 -0800

    All logos on Navbars now link to Home page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Container, Navbar, Form, Button } from "react-bootstrap";
 import React from 'react';
 import styled from 'styled-components';
 import Logo from "../assets/logo.jpg"; 
+import { Link } from "react-router-dom";
 
 const Styles = styled.div`
 
@@ -28,28 +29,30 @@ export const NavigationBar = () => (
     <Styles className="sticky-top">
         <Navbar variant='dark' sticky="top" controls={false}>
             <Container>
-                        <Navbar.Brand>
-                            <img 
-                             src={Logo}
-                             width="90"
-                             height="85"
-                             className="d-inline-block align-middle"
-                             alt=""
-                             />
-                             Twelfth Man
-                        </Navbar.Brand>
-                        <Form className="d-flex w-50">
-                            <Form.Control
-                            type="search"
-                            placeholder="Search for a team or tournament"
-                            className='mr-sm-2'
-                            />
-                            <Button type='submit'>Search</Button>
-                        </Form>
-                        <Form className='d-flex'>
-                            <Button type='button' variant='outline-success' href='#login'>Login</Button>
-                            <Button type='button' variant='outline-success' href='#sign-up'>Sign Up</Button>
-                        </Form>
+                <Link to="/"> 
+                    <Navbar.Brand>
+                        <img 
+                        src={Logo}
+                        width="90"
+                        height="85"
+                        className="d-inline-block align-middle"
+                        alt=""
+                        />
+                        Twelfth Man
+                    </Navbar.Brand>
+                </Link>
+                <Form className="d-flex w-50">
+                    <Form.Control
+                    type="search"
+                    placeholder="Search for a team or tournament"
+                    className='mr-sm-2'
+                    />
+                    <Button type='submit'>Search</Button>
+                </Form>
+                <Form className='d-flex'>
+                    <Button type='button' variant='outline-success' href='login'>Login</Button>
+                    <Button type='button' variant='outline-success' href='sign-up'>Sign Up</Button>
+                </Form>
             </Container>
         </Navbar>
     </Styles>
--- frontend/src/components/NavbarTournament.js
+++ frontend/src/components/NavbarTournament.js
@@ -2,6 +2,7 @@ import { Container, Navbar, Form, Button } from "react-bootstrap";
 import React from 'react';
 import styled from 'styled-components';
 import Logo from "../assets/logo.jpg"; 
+import { Link } from "react-router-dom";
 
 const Styles = styled.div`
 
@@ -28,24 +29,26 @@ export const NavigationBar = () => (
     <Styles className="sticky-top">
         <Navbar variant='dark' sticky="top">
             <Container>
-                        <Navbar.Brand>
-                            <img 
-                             src={Logo}
-                             width="90"
-                             height="85"
-                             className="d-inline-block align-middle"
-                             alt=""
-                             />
-                             Twelfth Man
-                        </Navbar.Brand>
-                        <Form className="d-flex w-50">
-                            <Form.Control
-                            type="search"
-                            placeholder="Search for a team or tournament"
-                            className='mr-sm-2'
-                            />
-                            <Button type='submit'>Search</Button>
-                        </Form>
+                <Link to="/"> 
+                    <Navbar.Brand>
+                        <img 
+                        src={Logo}
+                        width="90"
+                        height="85"
+                        className="d-inline-block align-middle"
+                        alt=""
+                        />
+                        Twelfth Man
+                    </Navbar.Brand>
+                </Link>
+                <Form className="d-flex w-50">
+                    <Form.Control
+                    type="search"
+                    placeholder="Search for a team or tournament"
+                    className='mr-sm-2'
+                    />
+                    <Button type='submit'>Search</Button>
+                </Form>
             </Container>
         </Navbar>
     </Styles>
