c1433a67dbf4537fe95ee69be251746bfacd0864
arnavmarda <arnavmarda@gmail.com>
2022-11-30 05:13:47 -0800

    fixed the user navbar

--- frontend/src/components/NavbarUser.js
+++ frontend/src/components/NavbarUser.js
@@ -1,15 +1,20 @@
 import { Container, Navbar, Form, Button, Nav } from "react-bootstrap";
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import Logo from "../assets/logo.jpg";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Logo from "../assets/logo.jpg"; 
 import { Link } from "react-router-dom";
 import { DropdownSearch } from "./DropdownSearch";
+import { useNavigate } from "react-router-dom";
 
 const Styles = styled.div`
+
     .nav-link {
-        color: #ffd100;
+        color: #FFD100;
         font-family: Georgia;
         padding-left: 25px;
+        text-decoration: none !important;
+    }
+    .a:link, .a:hover, .a: visited, .a:active {
         text-decoration: none;
     }
 
@@ -17,94 +22,73 @@ const Styles = styled.div`
         background-color: #005587;
         height: 75px;
     }
-
+    
     .btn {
-        background-color: #ffb81c;
+        background-color: #FFB81C;
         color: black;
         margin: 3px;
     }
 `;
 
+
 export const NavigationBar = () => {
+
+    var navigate = useNavigate();
+
     const [searchValue, setSearchValue] = useState("");
 
     const handleSearch = (searchValue) => {
         setSearchValue(searchValue);
-    };
+        var goTo = `/${searchValue.value}`;
+        navigate(goTo);
+    }
 
     const searchList = [
-        { values: "team", label: "Team 1" },
-        { values: "team", label: "Team 2" },
-        { values: "team", label: "Team 3" },
-        { values: "tournament", label: "Tournament 1" },
-        { values: "tournament", label: "Tournament 2" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
-        { values: "tournament", label: "Tournament 3" },
+        {value: "team", label: "Team 1"},
+        {value: "team", label: "Team 2"},
+        {value: "team", label: "Team 3"},
+        {value: "tournament", label: "Tournament 1"},
+        {value: "tournament", label: "Tournament 2"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
+        {value: "tournament", label: "Tournament 3"},
     ];
 
-    return (
+    return(
         <Styles className="sticky-top">
-            <Navbar className="starter" variant="dark" sticky="top">
+            <Navbar variant='dark' sticky="top" controls={false}>
                 <Container>
                     <Nav.Link>
-                        <Link to="/">
-                            <img
-                                src={Logo}
-                                width="90"
-                                height="85"
-                                className="d-inline-block align-middle"
-                                alt=""
+                        <Link to="/"> 
+                            <img 
+                            src={Logo}
+                            width="90"
+                            height="85"
+                            className="d-inline-block align-middle"
+                            alt=""
                             />
                         </Link>
                         Twelfth Man
                     </Nav.Link>
+                    
                     <Form className="d-flex w-50">
-                        <DropdownSearch
-                            options={searchList}
-                            handleChange={handleSearch}
-                            selectedOption={searchValue}
-                            placeholder={"Search for a team or tournament"}
-                        />
+                        <DropdownSearch options={searchList} handleChange={handleSearch} selectedOption={searchValue} placeholder={"Search for a team or tournament"} />
                     </Form>
-                    <Form className="d-flex">
-                        <Link to="/createteam">
-                            <Button
-                                type="button"
-                                variant="outline-success"
-                                href="#create-team"
-                            >
-                                Create Team
-                            </Button>
-                        </Link>
-                        <Link to="/createtournament">
-                            <Button
-                                type="button"
-                                variant="outline-success"
-                                href="#create-team"
-                            >
-                                Create Tournament
-                            </Button>
-                        </Link>
-                        <Link to="/creatematch">
-                            <Button
-                                type="button"
-                                variant="outline-success"
-                                href="#create-team"
-                            >
-                                Create Match
-                            </Button>
-                        </Link>
-                    </Form>
+
+                    <Form className='d-flex'>
+                        <Link to="/creatematch"><Button type='button' variant='outline-success' href='login'>Create Match</Button></Link>
+                        <Link to="/createteam"><Button type='button' variant='outline-success' href='sign-up'>Create Team</Button></Link>
+                        <Link to="/createtournament"><Button type='button' variant='outline-success' href='sign-up'>Create Tournament</Button></Link>
+                    </Form>                
                 </Container>
             </Navbar>
         </Styles>
-    );
-};
+    )
+}
