665d7b76ec53115100bbcd61741c7a49031b50f1
Angad Kapoor <angadk0810@gmail.com>
2022-11-30 00:44:04 -0800

    fixed link to be anchored to

--- frontend/src/components/NavbarUser.js
+++ frontend/src/components/NavbarUser.js
@@ -1,15 +1,13 @@
 import { Container, Navbar, Form, Button, Nav } from "react-bootstrap";
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Logo from "../assets/logo.jpg"; 
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.jpg";
 import { Link } from "react-router-dom";
 import { DropdownSearch } from "./DropdownSearch";
 
-
 const Styles = styled.div`
-
     .nav-link {
-        color: #FFD100;
+        color: #ffd100;
         font-family: Georgia;
         padding-left: 25px;
         text-decoration: none;
@@ -19,68 +17,90 @@ const Styles = styled.div`
         background-color: #005587;
         height: 75px;
     }
-    
+
     .btn {
-        background-color: #FFB81C;
+        background-color: #ffb81c;
         color: black;
         margin: 3px;
     }
 `;
 
-
 export const NavigationBar = () => {
-
     const [searchValue, setSearchValue] = useState("");
 
     const handleSearch = (searchValue) => {
         setSearchValue(searchValue);
-    }
-
+    };
 
     const searchList = [
-        {values: "team", label: "Team 1"},
-        {values: "team", label: "Team 2"},
-        {values: "team", label: "Team 3"},
-        {values: "tournament", label: "Tournament 1"},
-        {values: "tournament", label: "Tournament 2"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
-        {values: "tournament", label: "Tournament 3"},
+        { values: "team", label: "Team 1" },
+        { values: "team", label: "Team 2" },
+        { values: "team", label: "Team 3" },
+        { values: "tournament", label: "Tournament 1" },
+        { values: "tournament", label: "Tournament 2" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
+        { values: "tournament", label: "Tournament 3" },
     ];
 
     return (
         <Styles className="sticky-top">
-            <Navbar className="starter" variant='dark' sticky="top">
+            <Navbar className="starter" variant="dark" sticky="top">
                 <Container>
                     <Nav.Link>
                         <Link to="/">
-                            <img 
+                            <img
                                 src={Logo}
                                 width="90"
                                 height="85"
                                 className="d-inline-block align-middle"
                                 alt=""
-                                />
+                            />
                         </Link>
-                            Twelfth Man
+                        Twelfth Man
                     </Nav.Link>
                     <Form className="d-flex w-50">
-                        <DropdownSearch options={searchList} handleChange={handleSearch} selectedOption={searchValue} placeholder={"Search for a team or tournament"} />
+                        <DropdownSearch
+                            options={searchList}
+                            handleChange={handleSearch}
+                            selectedOption={searchValue}
+                            placeholder={"Search for a team or tournament"}
+                        />
                     </Form>
-                    <Form className='d-flex'>
-                        <Link to="/TeamCreation"><Button type='button' variant='outline-success' href='#create-team'>Create Team</Button></Link>
-                        <Button type='button' variant='outline-success' href='#create-tournament'>Create Tournament</Button>
-                        <Button type='button' variant='outline-success' href='#create-tournament'>Home</Button>
+                    <Form className="d-flex">
+                        <Link to="/createteam">
+                            <Button
+                                type="button"
+                                variant="outline-success"
+                                href="#create-team"
+                            >
+                                Create Team
+                            </Button>
+                        </Link>
+                        <Button
+                            type="button"
+                            variant="outline-success"
+                            href="#create-tournament"
+                        >
+                            Create Tournament
+                        </Button>
+                        <Button
+                            type="button"
+                            variant="outline-success"
+                            href="#create-tournament"
+                        >
+                            Home
+                        </Button>
                     </Form>
                 </Container>
             </Navbar>
         </Styles>
-    )
-}
+    );
+};
