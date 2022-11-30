8d2c2c23257419bad2260507393638851f5172a8
Angad Kapoor <angadk0810@gmail.com>
2022-11-29 17:59:48 -0800

    added necessary imports for schemas and routes

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,17 @@ const { MONGO_URI } = require("./keys.js");
 
 app.use(express.json());
 
+require("./models/user");
+require("./models/match");
+require("./models/scorecard");
+require("./models/team");
+require("./models/tournament");
+
+app.use(require("./routes/matches.js"));
+app.use(require("./routes/teams.js"));
+app.use(require("./routes/tournaments.js"));
+app.use(require("./routes/users.js"))
+
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
