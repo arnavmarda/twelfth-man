510d8a81d956eac5e28ecfde9633e6aa32124472
Angad Kapoor <angadk0810@gmail.com>
2022-11-29 17:20:04 -0800

    changes to allow mongoose connection

---
+++ backend/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    MONGO_URI: "mongodb+srv://angad:2pLJXOUjJDzE0BoQ@cluster0.glgaslu.mongodb.net/?retryWrites=true&w=majority",
+    SECRET_KEY: "lightningsharks"
+}
+// Anirudh: tHPCSmIyJxthTqa3
+// Angad: wCLGzUCQUSVk4osI
+
+/*
+    new: angad -> 2pLJXOUjJDzE0BoQ
+
+*/
